634f9695179888cabeb949d9394326c72bd70e15
Laura Eck <laura.tatsutahime@gmail.com>
2014-05-31 23:18:50 +0200

    basic search, more styles

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,15 @@
 class Project < ActiveRecord::Base
   validates :name, :url, :language, :description, presence: true
+
+  def self.search(search_params)
+    puts "I'm here"
+    puts search_params
+    if search_params
+      search_params.downcase!
+      projects = Project.where("language LIKE ?", "%#{search_params}%")
+    else
+      puts "second branch"
+      projects = Project.all
+    end
+end
 end
