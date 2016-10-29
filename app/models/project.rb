86631e4368e41cfbd9a78ad10d72dc377fc09217
Laura <laura.tatsutahime@gmail.com>
2016-10-29 16:28:07 +0900

    Add path method to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -5,4 +5,10 @@ class Project < ActiveRecord::Base
   has_many :languages, through: :project_languages
   has_many :issues
 
+  def path
+    project_path = URI.parse(self.url).path
+    project_path.slice!(0) if project_path[0] == "/"
+    project_path
+  end
+
 end
