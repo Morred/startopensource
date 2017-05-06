7a05ef98e0135db50900c330d16a1d62cd0d6bf7
Laura <laura.tatsutahime@gmail.com>
2017-05-06 12:31:57 +0900

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+ruby = Language.create(name: 'Ruby')
+elixir = Language.create(name: 'Elixir')
+js = Language.create(name: 'JavaScript')
+c = Language.create(name: 'C')
+
+project_cupcake = Project.create(
+  name: 'Project Cupcake',
+  url: 'http://www.github.com/cupcaketown/cupcake',
+  description: 'The best cupcakes in town'
+)
+
+project_muffin = Project.create(
+  name: 'Project Muffin',
+  url: 'http://www.github.com/muffincity/muffin',
+  description: 'Muffins you dream about'
+)
+
+Issue.create(
+  project: project_cupcake,
+  languages: [ruby, js],
+  title: 'Apply frosting',
+  github_issue_id: '123',
+  url: 'http://www.github.com/cupcaketown/cupcake/issues/123'
+)
+
+Issue.create(
+  project: project_cupcake,
+  languages: [js],
+  title: 'Add sparkles',
+  github_issue_id: '124',
+  url: 'http://www.github.com/cupcaketown/cupcake/issues/124'
+)
+
+Issue.create(
+  project: project_muffin,
+  languages: [elixir],
+  title: 'Stir in chocolate chips',
+  github_issue_id: '35',
+  url: 'http://www.github.com/cupcaketown/muffin/issues/35'
+)
